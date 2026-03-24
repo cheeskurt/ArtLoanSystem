fcd82600063ce1a1d7781b50869ed1ea926600ce
Magnus Lopez <ac148904@avcol.school.nz>
2026-03-25 12:24:03 +1300

    Modified user model created by Identity scaffolding.

--- WebApplication3/Areas/Identity/Data/User.cs
+++ WebApplication3/Areas/Identity/Data/User.cs
@@ -7,7 +7,18 @@ using Microsoft.AspNetCore.Identity;
 namespace WebApplication3.Areas.Identity.Data;
 
 // Add profile data for application users by adding properties to the User class
+
+public enum Class 
+{ 
+
+}
+
 public class User : IdentityUser
 {
+    public int UserID { get; set; }
+    public string AC { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public Class Class { get; set; }
 }
 
