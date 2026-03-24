277236ed431a74e13c026d13fe2b8382ca4bb48f
Magnus Lopez <ac148904@avcol.school.nz>
2026-03-25 12:14:50 +1300

    Created Issue model

---
+++ WebApplication3/Models/Issue.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public enum Status
+    {
+         
+    }
+    public class Issue
+    {
+        public int IssueID { get; set; }     
+        public Status Status { get; set; }
+        public DateTime DateIssued { get; set; }
+        public DateTime DateReturned { get; set; }
+        public DateTime DateDue { get; set; }
+
+        public ICollection<>
+    }
+}
