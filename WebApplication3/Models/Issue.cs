5f5f9837554deff8edcdcfc1e88fd73b460fc359
cheese <163073438+cheeskurt@users.noreply.github.com>
2026-03-31 13:23:50 +1300

    Removed the status field from the Issue model.

--- WebApplication3/Models/Issue.cs
+++ WebApplication3/Models/Issue.cs
@@ -3,14 +3,9 @@ using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication3.Models
 {
-    public enum Status
-    {
-         
-    }
     public class Issue
     {
         public int IssueID { get; set; }     
-        public Status Status { get; set; }
         public DateTime DateIssued { get; set; }
         public DateTime DateReturned { get; set; }
         public DateTime DateDue { get; set; }
