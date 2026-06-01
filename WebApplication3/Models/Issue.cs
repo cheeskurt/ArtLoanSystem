66a4ba9b1961ee7ca8b93bacf367856abd1f66b3
cheese <163073438+cheeskurt@users.noreply.github.com>
2026-06-02 10:38:14 +1200

    Also added [Key] annotations to respective primary keys.

--- WebApplication3/Models/Issue.cs
+++ WebApplication3/Models/Issue.cs
@@ -6,7 +6,7 @@ namespace WebApplication3.Models
     public class Issue
     {
         [Key]
-        public int IssueID { get; set; }
+        public int IssueID { get; set; } 
         public DateTime DateIssued { get; set; }
         public DateTime DateReturned { get; set; }
         public ICollection<ItemIssue>? ItemIssues { get; set; }
